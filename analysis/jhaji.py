131fe583f7fb507de584d2920ede8bb4bf96d48e
Arshpreet Kaur <akdhanjal2000@gmail.com>
2023-04-15 21:37:55 +0530

    Update jhaji.py

--- analysis/jhaji.py
+++ analysis/jhaji.py
@@ -1,12 +1,26 @@
+#import relevant libraries
+
 import pandas as pd
+import numpy as np
+import os
+from IPython.display import display
+#import qgrid
 
-df = pd.read_csv('Checkmarks.csv')
+# # Set display options for pandas dataframes
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.width', None)
+# pd.set_option('display.max_rows', None)
 
+#import csv file using pandas
+data = pd.read_csv('C:/Users/admin/Documents/GitHub/dailyHabitsAppCSVAnalysis/Checkmarks.csv')
+cols = data.columns[:-1]
+#print(cols)
+#print(data.head())
 
+row=data.iloc[0]
+print(row)
 
-row1 = df.iloc[1,0]
+# new_data = data.iloc[:, ::2]
+# print(new_data.head())
 
-
-print("Hi")
-print(row1)
 
